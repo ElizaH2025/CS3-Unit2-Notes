1f14685bab60985d4d59d5544269bde16d76aa5f
ElizaH2025 <eliza.horowitz2025@gbwl.org>
2024-10-01 19:11:36 +0000

    data selection

--- main.py
+++ main.py
@@ -38,5 +38,43 @@ cookies_dict = {'Double Chocolate:': 5,
 cookies = pd.Series(cookies_dict)
 print(cookies)
 
+print()
 # Access utem by index
-print('Rating od dirt cookies: ', cookies['DIRT:'])
+print('Rating of dirt cookies: ', cookies['DIRT:'])
+print()
+
+# DataFrame is like a 2D array or specialized dictionary
+cookies_df = pd.DataFrame(cookies, columns=['Ratings'])
+print(cookies_df)
+
+# Add a column to our DataFrame
+cookies_df['Allergens'] = ['True','True', 'True', 'True', 'False']
+print(cookies_df)
+
+print()
+
+# Another way to add a column
+cookies_df['sweetness'] = {'Double Chocolate:': 10,
+                'Chocolate Chip:': 8.5,
+                'Oatmeal Raisin:': 7,
+                'Snickerdoodle:': 8,
+                'DIRT:': -1 }
+
+print(cookies_df)
+
+print()
+
+print('****** DATA SELECTION ******')
+data = pd. Series(['E','l','i','z', 'a'], index=[5, 12, 8, 26, 1])
+
+# Indexing uses the explicit index
+print(data[5])
+
+#Slicing (getting multiples values) uses IMPLICIT index
+print(data[2:4]) #not getting expected output
+
+# Instead, use the LOC attribute to get a slice that uses explicit indices
+print(data.loc[5:1]) #includes items at index 5
+
+# Not as common, iLoc uses implicit indices
+print(data,iloc[5:1]) # doesn't include second item
